5783810043779b56ed215de98ce19a199dd4cdd7
Diego Saldaña <saldana_d@outlook.com>
2026-03-12 01:41:08 -0600

    Revert "Feature/unity"

--- apps/web/src/services/matchesService.ts
+++
@@ -1,6 +0,0 @@
-import { apiFetch } from "./api";
-import type { ApiMatch } from "../types";
-
-export async function getMatches(): Promise<ApiMatch[]> {
-  return await apiFetch<ApiMatch[]>("/matches/");
-}
--- services/matches-service/src/db.ts
+++
@@ -1,8 +0,0 @@
-import { Pool } from "pg";
-
-const pool = new Pool({
-  connectionString: process.env.MATCHES_DB_URL,
-  ssl: false,
-});
-
-export { pool };
